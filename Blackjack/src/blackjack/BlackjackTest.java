d4d8aa89889a1a95677b8d1c2d01ef4732ee785f
Jacob Slack <jslack3@bruinmail.slcc.edu>
2020-11-19 20:53:30 -0700

    Updates Hand class. Adds subclasses for DealerHand and PlayerHand.

--- Blackjack/src/blackjack/BlackjackTest.java
+++ Blackjack/src/blackjack/BlackjackTest.java
@@ -1,7 +1,14 @@
+/**
+ *
+ */
 package blackjack;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
- * Tests the classes of package blackjack for development.  
+ * Tests the classes of package blackjack for development.
  * Not intended for production use.
  * @author Edward Szetela
  *
@@ -10,8 +17,50 @@ public class BlackjackTest {
 
 	public static void main(String[] args) {
 		Deck cardDeck = new Deck();
-		cardDeck.printDeck();
-		
+		ArrayList<Card> playerInitialHand = new ArrayList<>();
+		ArrayList<Card> dealerInitialHand = new ArrayList<>();
+
+		dealHand(cardDeck, playerInitialHand);
+		dealHand(cardDeck, dealerInitialHand);
+
+		Hand playerHand = new PlayerHand(0, playerInitialHand);
+		Hand dealerHand = new DealerHand(0, dealerInitialHand);
+
+		System.out.println("playerHandScore: " + playerHand.getHandScore());
+		playerHand.getCardsInHand().forEach(c -> System.out.println(c.getFace() + c.getSuit()));
+
+		System.out.println("dealerHandScore: " + dealerHand.getHandScore());
+		dealerHand.getCardsInHand().forEach(c -> System.out.println(c.getFace() + c.getSuit()));
+
+		System.out.println();
+
+		//hit
+		boolean hit = true;
+		if (hit) {
+			Card tempCard = cardDeck.drawCard();
+			playerHand.addCard(tempCard);
+		}
+
+		System.out.println("playerHandScore after player hit: " + playerHand.getHandScore());
+		playerHand.getCardsInHand().forEach(c -> System.out.println(c.getFace() + c.getSuit()));
+
+		System.out.println("dealerHandScore still same after player hit?: " + dealerHand.getHandScore());
+		dealerHand.getCardsInHand().forEach(c -> System.out.println(c.getFace() + c.getSuit()));
+
+
+		/* Think we need to make the update method and the gameState field static
+		 *  on the Match class but we can discuss tomorrow
+		 */
+		// boolean playerStand = false;
+		// Match.update(dealerHand, playerHand);
+
 	}
+
+	private static void dealHand(Deck cardDeck, ArrayList<Card> hand) {
+		for (int i = 0; i < 2; i++) {
+			hand.add(cardDeck.drawCard());
+		}
+	}
+
 
 }
---
+++ Blackjack/src/blackjack/DealerHand.java
@@ -0,0 +1,12 @@
+package blackjack;
+
+import java.util.ArrayList;
+
+public class DealerHand extends Hand {
+
+	public DealerHand(int handScore, ArrayList<Card> cardsInHand) {
+		super(handScore, cardsInHand);
+		// TODO Auto-generated constructor stub
+	}
+
+}
