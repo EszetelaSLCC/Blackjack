7f32089ac6d8275a51e2a22b68d5ff1e4b1af37e
eszetela <eszetela@6JD6MH2.skywest.com>
2020-11-20 10:58:39 -0800

    Change BlackJack test to mimic game
    The Hand classes look good and work correctly.  I redid the test class
    to somewhat play a game.  You have to stand repeatedly and ignore the
    dealer's deal state but it seems to work.

--- Blackjack/src/blackjack/BlackjackTest.java
+++ Blackjack/src/blackjack/BlackjackTest.java
@@ -4,48 +4,99 @@
 package blackjack;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Scanner;
 
 /**
  * Tests the classes of package blackjack for development.
  * Not intended for production use.
- * @author Edward Szetela
+ * @author Edward Szetela & Jacob Slack
  *
  */
 public class BlackjackTest {
 
 	public static void main(String[] args) {
+		
 		Deck cardDeck = new Deck();
 		ArrayList<Card> playerInitialHand = new ArrayList<>();
 		ArrayList<Card> dealerInitialHand = new ArrayList<>();
 
+		Match gameMatch = new Match();
+		GameState state;
+		
+		@SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
+		
 		dealHand(cardDeck, playerInitialHand);
 		dealHand(cardDeck, dealerInitialHand);
-
+		
 		Hand playerHand = new PlayerHand(0, playerInitialHand);
 		Hand dealerHand = new DealerHand(0, dealerInitialHand);
-
-		System.out.println("playerHandScore: " + playerHand.getHandScore());
+		
+		System.out.println("playerHandScore on deal: " + playerHand.getHandScore());
 		playerHand.getCardsInHand().forEach(c -> System.out.println(c.getFace() + c.getSuit()));
 
-		System.out.println("dealerHandScore: " + dealerHand.getHandScore());
+		System.out.println("dealerHandScore on deal: " + dealerHand.getHandScore());
 		dealerHand.getCardsInHand().forEach(c -> System.out.println(c.getFace() + c.getSuit()));
+		
+		state = gameMatch.update(dealerHand, playerHand, false);
+		System.out.println(state);
 
 		System.out.println();
 
 		//hit
-		boolean hit = true;
-		if (hit) {
-			Card tempCard = cardDeck.drawCard();
-			playerHand.addCard(tempCard);
-		}
+		//boolean hit = true;
+		//if (hit) {
+			//Card tempCard = cardDeck.drawCard();
+			//playerHand.addCard(tempCard);
+		//}
 
-		System.out.println("playerHandScore after player hit: " + playerHand.getHandScore());
-		playerHand.getCardsInHand().forEach(c -> System.out.println(c.getFace() + c.getSuit()));
+	
+		do {
+			// ask player
+			System.out.println("1 to hit, 2 to stand:  ");
+			int playerChoice = input.nextInt();
+			boolean playerStanding = false;
+			
+			if (playerChoice == 1) {
+				// player hit
+				Card playerCard = cardDeck.drawCard();
+				playerHand.addCard(playerCard);
+				playerStanding = false;
+			}
+			else if (playerChoice == 2) {
+				// player stands
+				playerStanding = true;
+			}
+			// dealer
+			if (dealerHand.getHandScore() < 18 && playerStanding) {
+				// dealer must hit with these conditions
+				Card dealerCard = cardDeck.drawCard();
+				dealerHand.addCard(dealerCard);
+			}
+			
+			System.out.println("playerHandScore: " + playerHand.getHandScore());
+			playerHand.getCardsInHand().forEach(c -> System.out.println(c.getFace() + c.getSuit()));
 
-		System.out.println("dealerHandScore still same after player hit?: " + dealerHand.getHandScore());
-		dealerHand.getCardsInHand().forEach(c -> System.out.println(c.getFace() + c.getSuit()));
+			if (!playerStanding) {
+				// player not standing, show only one dealer card
+				System.out.println("dealerHandScore hidden");
+				System.out.println(dealerHand.getCardsInHand().get(0).getFace() + 
+						dealerHand.getCardsInHand().get(0).getSuit());
+			}
+			
+			if (playerStanding) {
+				// player standing, show dealer cards and score
+				System.out.println("dealerHandScore: " + dealerHand.getHandScore());
+				dealerHand.getCardsInHand().forEach(c -> System.out.println(c.getFace() + c.getSuit()));
+			}
+		
+			state = gameMatch.update(dealerHand, playerHand, playerStanding);
+			System.out.println(state + "\n");
+		} while (state == GameState.PLAYING);
+
+		//cardDeck.printDeck();
+		//cardDeck.shuffle(playerHand, dealerHand);
+		//cardDeck.printDeck();
 
 
 		/* Think we need to make the update method and the gameState field static
